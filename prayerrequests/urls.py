87bdaebbdc650d2686b44ce3d63efa1193e1090c
James Avery King <jamesaveryking@Jamess-MacBook-Pro.local>
2022-01-07 15:48:34 -0500

    UI Testing and Updates

---
+++ prayerrequests/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.index, name = 'index'),
+	path('specifics/<int:prayer_id>/', views.detail, name='prayerrequest'),
+	path('example/', views.example, name = 'example'),
+]
---
+++ prayerrequests/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Prayer
+from django.template import loader
+# Create your views here.
+
+def index(request):
+	latest_prayer_list = Prayer.objects.all()
+	template = loader.get_template('prayerrequests/index.html')
+	context = {
+		'latest_prayer_list' : latest_prayer_list
+	}
+	return HttpResponse(template.render(context,request))
+
+def detail(request, prayer_id):
+	try:
+		prayer = Prayer.objects.get(pk=prayer_id)
+	except Prayer.DoesNotExist:
+		raise Http404('Prayer ID not located')
+	return render(request, 'prayerrequests/detail.html', {'prayer' : prayer})
+
+def example(request):
+	template = loader.get_template('prayerrequests/components00.html')
+	context = {}
+	return HttpResponse(template.render(context,request))
